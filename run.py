781b9287cfa5f5f61694dfd189c88274efd8289d
HJX <1091408982@qq.com>
2023-06-08 17:07:41 +0800

    test

--- run.py
+++ run.py
@@ -27,13 +27,13 @@ class Item(BaseModel):
     q:str
 
 @app.post("/chat/")
-async def chatgpt(item:Item):
+def chatgpt(item:Item):
     print("收到"+item.q)
     return util.review_qusetion(item.q)
     # return item.q
 
 @app.get("/chat")
-async def new_chat():
+def new_chat():
     util.content = [{'role': 'system', 'content': '你是一擅长做阅读理解的助手'}]
     util.user_content = []
     return "ok"
